658626f821d401101f4aeaaa779d17c0fbe17388
kiran <gandikotakiran99@gmail.com>
2019-12-26 21:41:58 +0530

    first commit

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,2 +1,32 @@
 import React,{Component} from 'react';
 import {Bar,Line,Pie} from 'react-chartjs-2';
+
+class Chart extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            chartData:{
+                labels:["AP","TN","TG","KA","KL","GOA"],
+                datasets:[{
+                    label:"Population",
+                    data:[500000,700000,400000,900000,200000,170000],
+                    backgroundColor:[
+                        'rgba(22,99,132,0.6)'
+                    ]
+                }]
+            }
+        }
+    }
+    render(){
+        return(
+            <div className="chart">
+                Chart Component
+                <Bar
+                    data={this.state.chartData}
+                    options={{ maintainAspectRatio: false }}
+                    />
+            </div>
+        )
+    }
+}
+export default Chart;
